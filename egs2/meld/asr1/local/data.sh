3c0e86b2b0e0a2557077e38c44cce2b3f8f488fc
David Bick <davidlbick@gmail.com>
2022-02-23 13:56:03 -0500

    Passing utils/validate_data_dir.sh for MELD.Raw data of MELD

--- egs2/meld/asr1/local/data.sh
+++ egs2/meld/asr1/local/data.sh
@@ -6,13 +6,19 @@ set -u
 set -o pipefail 
 
 log() {
-  local fname=${BASH_SOURCE[1]##*/}
-  echo -e "$(data '+%Y-%m-%dT%H:%M:%S') (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $*" 
+     local fname=${BASH_SOURCE[1]##*/}
+     echo -e "$(date '+%Y-%m-%dT%H:%M:%S') (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $*"
 }
 
+
 SECONDS=0
-# TODO will replace this eventually once we move it onto Bhiksha's project
-datadir=/ocean/projects/iri120008p/roshansh/corpora/meld
+# TODO can I just remove this somehow
+stage=1
+stop_stage=100
+# TODO replace this with PSC after done testing 
+#datadir=/ocean/projects/cis220008p/shared/meld
+# temporary to test on workhorse
+datadir=/home/dbick/workhorse1/data/meld
 # TODO add switch for this vs FriendsPersona
 sub_dataset=MELD.Raw
 # sub_dataset=FriendsPersona
@@ -33,7 +39,7 @@ log "$0 $*"
 . ./path.sh  # DOUBT seems to just update some paths to allow access to Kaldi 
 . ./cmd.sh   # DOUBT seems that it sets run.pl to be the default file when backend is local, similar for slurm.pl if slurm is backend etc
 
-if [${stage} -le 1] && [${stop_stage} -ge 1]; then
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
   log "stage 1: MELD Data Preparation"
   #TODO
   # utt2spk
@@ -43,40 +49,38 @@ if [${stage} -le 1] && [${stop_stage} -ge 1]; then
   # segments 
   tmp=tmp
   # if hasn't been already 
-  if [! -e data/${tmp}/tmp.done]; then
+  if [ ! -e data/${tmp}/tmp.done ]; then
     # create directories and files for Kaldi-style data prep
+    rm -r data/{train,valid,test}
     mkdir -p data/{train,valid,test}
-    mkdir -p data/${tmp}
-    echo -n "" > data/${tmp}/wav.scp 
-    echo -n "" > data/${tmp}/utt2spk 
-    echo -n "" > data/${tmp}/text
-    echo -n "" > data/${tmp}/segments
     
+    python local/create_speaker_id.py --fpath "${datadir}/${sub_dataset}/train_sent_emo.csv"
     # loop through datadir and collect all the needed information 
+    data_dir=(train valid test)
     label_files=(train_sent_emo.csv dev_sent_emo.csv test_sent_emo.csv)
     audio_dirs=(train_splits dev_splits_complete output_repeated_splits_test)
     for i in ${!label_files[@]}; do
       label_file=${label_files[$i]}
       audio_dir=${audio_dirs[$i]}
+      out_dir=${data_dir[$i]}
+      
+      python local/parse_csv.py --label-file "${datadir}/${sub_dataset}/${label_file}" --audio-dir "${datadir}/${sub_dataset}/${audio_dir}" --wav-scp "data/${out_dir}/wav.scp" --utt2spk "data/${out_dir}/utt2spk" --text "data/${out_dir}/text"
 
-      # below is just looping through values of the CSV's, code adapted from https://www.baeldung.com/linux/csv-parsing
-      while IFS="," read -r idk_TODO utt_text spkr emo sntmnt dia_ID utt_ID season episode strt_tme end_tme 
-      do 
-        audio_file="dia${dia_ID}_utt${utt_ID}.mp4" 
-      done < <(tail -n +2 ${datadir}/${sub_dataset}/${label_file})  # skips the header of csv file
+      utils/utt2spk_to_spk2utt.pl data/${out_dir}/utt2spk > "data/${out_dir}/spk2utt"
 
     done
 
     # write the needed info to appropriate place: text, wav.scp, utt2spk, and segments
 
-    # TODO make sure these are the right things to do 
-    dos2unix data/${tmp}/wav.scp; dos2unix data/${tmp}/utt2spk; dos2unix data/${tmp}/text
-    utils/utt2spk_to_spk2utt.pl data/${tmp}/utt2spk > "data/${tmp}/spk2utt"
-    touch data/${tmp}/tmp.done
+    # TODO dos2unix command not found, is it necessary  
+    # dos2unix data/${out_dir}/wav.scp; dos2unix data/${out_dir}/utt2spk; dos2unix data/${out_dir}/text
+    # TODO add this back in when done
+    #touch data/${out_dir}/tmp.done
   fi
 fi
 
 for dset in test valid train; do 
+  utils/fix_data_dir.sh data/${dset}
   utils/validate_data_dir.sh --no-feats data/${dset} || exit 1
 done 
 
---
+++ egs2/meld/asr1/run.sh
@@ -0,0 +1,8 @@
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+./asr.sh \
+    --lang en \
