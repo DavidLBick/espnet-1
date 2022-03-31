90e4923e00224e5a58e18bca6a879e525c2e3ba2
roshansh-cmu <roshansh@andrew.cmu.edu>
2022-03-31 12:15:24 -0400

    IEMOCAP Update

--- espnet2/asr/decoder/seq_classification.py
+++ espnet2/asr/decoder/seq_classification.py
@@ -1,52 +1,50 @@
 import torch
 import torch.nn.functional as F
-from typeguard import check_argument_types 
-
-from espnet2.asr.decoder.abs_decoder import AbsDecoder 
-from espnet2.utils.get_default_kwargs import get_default_kwargs 
+from typeguard import check_argument_types
+import logging
+from espnet2.asr.decoder.abs_decoder import AbsDecoder
+from espnet2.utils.get_default_kwargs import get_default_kwargs
 from espnet.nets.pytorch_backend.nets_utils import to_device
 
+
 class SeqClassifier(AbsDecoder):
-  def __init__(
-    self, 
-    vocab_size, 
-    encoder_output_size, 
-    pool_type,
-    attention_heads,
-    embed_dim
-  ):
-    super().__init__()
-    self.pool_type = pool_type
-    if pool_type == 'att':
-      self.query = nn.Embedding(encoder_output_size, embed_dim) 
-      self.value = nn.Linear(encoder_output_size, embed_dim) 
-      self.key = nn.Linear(encoder_output_size, embed_dim)
-      self.mha = nn.MultiheadAttention(embed_dim, attention_heads)
+    def __init__(
+        self,
+        vocab_size: int,
+        encoder_output_size: int,
+        pool_type: str = "mean",
+        attention_heads: int = 4,
+        attention_dim: int = 512,
+        dropout_rate: float = 0.0,
+    ):
+        super().__init__()
+        self.pool_type = pool_type
+        if pool_type == "att":
+            self.query = torch.nn.Embedding(encoder_output_size, attention_dim)
+            self.value = torch.nn.Linear(encoder_output_size, attention_dim)
+            self.key = torch.nn.Linear(encoder_output_size, attention_dim)
+            self.mha = torch.nn.MultiheadAttention(attention_dim, attention_heads)
 
-    self.logistic = nn.Linear(encoder_output_size, vocab_size)
-    self.softmax = nn.Softmax(dim=-1)
-  
-  def pool(hs_pad, ys_in_pad):
-    if self.pool_type == 'att':
-      key, query, value = self.key(hs_pad), self.query(ys_in_pad), self.value(hs_in_pad)
-      pooled, att = self.multihead_att(key, query, value) 
+        self.output = torch.nn.Linear(encoder_output_size, vocab_size)
 
-    elif self.pool_type == 'mean':
-      pooled, att = hs_pad.mean(dim=1), None
+    def pool(self, hs_pad, hlens):
+        if self.pool_type == "att":
+            key, query, value = (
+                self.key(hs_pad),
+                self.query(hs_pad),
+                self.value(hs_pad),
+            )
+            pooled, att = self.multihead_att(key, query, value)
 
-    elif self.pool_type == 'max':
-      pooled, att = hs_pad.max(dim=1), None
+        elif self.pool_type == "mean":
+            pooled, att = hs_pad.mean(dim=1).unsqueeze(1), None
 
-    return pooled, att
+        elif self.pool_type == "max":
+            pooled, att = hs_pad.max(dim=1).unsqueeze(1), None
 
-  def forward(self, hs_pad, hlens, ys_in_pad, ys_in_lens):
-    print(hs_pad)
-    print(hlens)
-    print(ys_in_pad)
-    print(ys_in_lens)
-    pooled, att = self.pool(hs_pad, ys_in_pad)
-    logits = self.softmax(self.logistic(pooled)) 
-    return logits
+        return pooled, att
 
+    def forward(self, hs_pad, hlens, ys_in_pad, ys_in_lens):
+        pooled, att = self.pool(hs_pad, hlens)
+        return self.output(pooled), ys_in_lens
 
-    
